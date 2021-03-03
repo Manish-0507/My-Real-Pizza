0a009a1eb2d01e21cf1058a61b4e00a75f3dd715
Mukesh <manishsharma1582000@gmail.com>
2021-03-03 21:15:38 +0530

    just made home page

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 3000;
 //path
 const viewsPath = path.join(__dirname, '/resources/views');
 
+
+//Assets (mtlb express server response deta h html response to use btana hoga ki sari cheeze kha rkhi h css,js and all so that it can include them too with html)
+app.use(express.static('public'));
+
+//routes
 app.get('/', (req, res) => {
     res.render('home')
 })
